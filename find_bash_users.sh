259e8b821259c9017c8d6a9aca7640987139bb5a
Mackedonec <mackedonec187@gmail.com>
2024-09-30 21:41:03 +0300

    UPD

--- find_bash_users.sh
+++ find_bash_users.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Скрипт для пошуку всіх користувачів, які використовують /bin/bash
+
+# Використовуємо команду grep для пошуку рядків з /bin/bash у файлі /etc/passwd
+
 grep "/bin/bash" /etc/passwd | cut -d: -f1
